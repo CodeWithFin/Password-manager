bd9d78f3ecd1b9c596bca60b303ebe70c392a33f
Finley <finley.mwachia12@gmail.com>
2024-01-31 22:58:46 +0300

    added input section

---
+++ pwd-manager.py
@@ -0,0 +1,11 @@
+pwd = input("What is the master password? ")
+
+while True:
+mode = input('would you like to add a new password or view existing ones(view, add) ')
+if mode == "view":
+    pass
+elif mode =="add":
+    pass
+else:
+    print("Invalid mode")
+    continue
