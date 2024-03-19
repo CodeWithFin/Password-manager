980d2fb60ec5124b565053594e67b69f3db2d752
Finley <finley.mwachia12@gmail.com>
2024-03-19 05:06:31 +0300

    add the add function

--- pwd-manager.py
+++ pwd-manager.py
@@ -1,11 +1,26 @@
-pwd = input("What is the master password? ")
+master_pwd = input("What is the Master password? ")
+
+
+def view():
+    pass
+
+def add():
+    name = input("Account Name: ")
+    pwd = input("Password: ")
+
+    with open('password.txt', 'a') as f:
+        f.write(name + " " + pwd )
+
 
 while True:
-mode = input('would you like to add a new password or view existing ones(view, add) ')
-if mode == "view":
-    pass
-elif mode =="add":
-    pass
-else:
-    print("Invalid mode")
-    continue
+    mode = input("Would you like to add a new password or view existing ones (view,add) ? press q to quit ?").lower()
+    if mode == "q":
+        break
+
+    if mode == "view":
+        view()
+    elif mode == "add":
+        add()
+    else:
+        print("Invalid mode.")
+        continue
